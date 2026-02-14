130f928f5f0b0d1e535cf6eaa79b20064fe22626
migue <migue@Mike>
2026-02-14 09:35:11 -0600

    Respository creation

---
+++ src/main/java/com/TechnicalTest/MSanchezTechinicalTest/MSanchezTechinicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.TechnicalTest.MSanchezTechinicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSanchezTechinicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSanchezTechinicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TechnicalTest/MSanchezTechinicalTest/MSanchezTechinicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechnicalTest.MSanchezTechinicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSanchezTechinicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
